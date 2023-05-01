a363e0bf7e6db81ac81da2297f0818731837b327
YamiHeike <koscielniak.z@gmail.com>
2023-05-01 23:32:10 +0200

    Styling of the soon-to-be invisible verse class div

---
+++ main.js
@@ -0,0 +1,7 @@
+//DOM variables
+
+const generator = {
+    button: document.querySelector('.generator-submit'),
+    input: document.querySelector('.generator'),
+    label: document.querySelector('label')
+}
