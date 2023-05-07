78e736728aaa9f6a09f8ffede670f125ea71a17f
YamiHeike <koscielniak.z@gmail.com>
2023-05-07 16:36:20 +0200

    Logic is done, let's leave the template verse for now, need to learn a bit more JavaScript theory to come up with a better solution. For now, everything works as intended, even if the code isn't perfect

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const contents = document.querySelector('.contents');
+const crossIcon = document.querySelector('#cross-icon-button').innerHTML;
+const templateVerse = document.querySelector('.verse');
+
+templateVerse.style.display = 'none';
+templateVerse.style.visibility = 'hidden';
 
 const generator = {
     button: document.querySelector('.generator-submit'),
@@ -7,22 +12,32 @@ const generator = {
     verse: document.querySelector('.generator-verse')
 }
 
+
 const getBibleVerse = async (bookName, chapterNum, verseNum) => {
     try {
         const res = await axios.get(`https://bible-api.com/${bookName} ${chapterNum}:${verseNum}`);
         const newDiv = document.createElement('div');
         const verseH3 = document.createElement('h3');
         const verseP = document.createElement('p');
+        const verseButton = document.createElement('button');
         verseH3.innerText = res.data.reference;
         verseP.innerText = res.data.text;
+        verseButton.innerHTML = crossIcon;
         newDiv.classList.add('verse');
         contents.appendChild(newDiv)
         newDiv.appendChild(verseH3);
         newDiv.appendChild(verseP);
+        newDiv.appendChild(verseButton);
+        verseButton.addEventListener('click', () => {
+            if(verseButton.innerHTML = crossIcon)
+            verseButton.parentElement.remove();
+        })
+        
         
     }
+    
     catch {
-        return 'An error occured. Enter a valid Bible Book Name'
+        return 'An error occured. Enter a valid address'
     }
 };
 
@@ -33,3 +48,4 @@ generator.button.addEventListener('click', function(e) {
     generator.chapter.value = '';
     generator.verse.value = '';
 })
+
